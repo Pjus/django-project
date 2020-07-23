0b359d0120c1fa8eb54fe03ca2b57c298991aaa4
Pjus <wnstjd117@naver.com>
2020-07-23 15:41:19 +0900

    add app

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import sqlite3
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def list(request):
+    data = request.GET.copy()
+    with sqlite3.connect("D:\\SD_python\\django\\django-project\\matchdb.sqlite3") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("select * from matches")
+        data['rows'] = cur.fetchall()
+    return render(request, 'links.html', context=data)
