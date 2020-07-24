4e4bedf1b143acb75a113efae28b3d3c1a3cc888
Pjus <wnstjd117@naver.com>
2020-07-24 17:51:22 +0900

    update

--- naver_news_rank/mariaDB.py
+++ naver_news_rank/mariaDB.py
@@ -1,8 +1,14 @@
 import pymysql
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+mydb = client.EPL
+collection = db.match_results
+findall = collection.find()
 
 db = pymysql.connect(host='localhost', port=3306, user='root', \
     passwd='tiger', db='yojulabdb', charset='utf8', autocommit=True)
 
 cursor = db.cursor(pymysql.cursors.DictCursor)
-cursor.execute("SELEC * from economic")
+print(cursor.execute("select * from economic"))
 
